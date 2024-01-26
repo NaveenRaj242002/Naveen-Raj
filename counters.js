302fa486093985b60676861f06868e28d8f11107
NaveenRaj242002 <157810624+NaveenRaj242002@users.noreply.github.com>
2024-01-26 19:39:19 +0530

    Add files via upload

---
+++ counters.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+
+class Counters extends Component {
+
+    render() { 
+        const { onReset, Counters, onDelete, onIncrement } = this.props;
+        return (
+            <div><button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>{this.props.counters.map(counter => (<Counter key={counter.id} onDelete={this.props.onDelete} onIncrement={this.props.onIncrement} counter={counter}/>))}
+                
+            </div>
+        );
+    }
+} 
+
+ 
+export default Counters ;
